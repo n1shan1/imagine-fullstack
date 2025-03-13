55d7b12ec6dd71a1518348ba2e45b0517a752c02
Nishant Dev <nishantdev@Nishants-MacBook-Air.local>
2025-03-13 16:59:47 +0530

    added-cors

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ const app = express();
 const PORT = process.env.PORT || 8080;
 
 //middlewares
-app.use(cors());
+app.use(cors({ origin: "*" }));
 app.use(express.json());
 app.use(morgan("combined"));
 conncetDB();
